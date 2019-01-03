b247fc22d0b4bfb1cd3e00dc78973e56ca071d66
ernarosa <46324544+ernarosa@users.noreply.github.com>
2019-01-03 16:19:35 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h3>Data Pendaftar</h3>
+		<?php
+			echo "Nama Lengkap : ".$_GET['nama_lengkap']."</br>";
+			echo "Password : ".$_GET['password']."</br>";
+			echo "Jenis Kelamin : ".$_GET['jenis_kelamin']."</br>";
+			echo "Hobi : ".$_GET['keahlian']."</br>";
+			echo "Asal Kota : ".$_GET['kota']."</br>";
+			echo "Alamat Lengkap : ".$_GET['alamat_lengkap']."</br>";
+		?>
+</body>
+</html>
